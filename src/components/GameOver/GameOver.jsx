aee9af56f446bd6a56ecd0635b29d7db686962f0
daleinen7 <daleinen@gmail.com>
2020-10-18 23:26:57 -0500

    final touches I guess

--- src/components/GameOver/GameOver.jsx
+++ src/components/GameOver/GameOver.jsx
@@ -4,16 +4,12 @@ import * as scoresAPI from '../../services/scores-api';
 
 const GameOver = (props) => {
 
-    async function getScores() {
-        const scores = await scoresAPI.getScores();
-    }
-
     return(
         <div className={styles.GameOver}>
         <div className={styles.notice}>GAME OVER</div>
         <button onClick={props.handleNewGame}>Play Again</button>
         <h3>Your High Score</h3>
-        <p>{props.user.score}</p>
+        <p>{props.user.highScore}</p>
         </div>
     )
 }
